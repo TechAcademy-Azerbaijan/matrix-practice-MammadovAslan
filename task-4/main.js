a5fc2a6754a65f491d701a5793ac5e5e25004cea
Aslan Mammadov <mammadovaslan21@gmail.com>
2022-06-18 01:00:11 +0400

    task

--- task-4/main.js
+++ task-4/main.js
@@ -1,5 +1,54 @@
 let n = 5;
-let i = 4;
-let j = 2;
+// let i = 4;
+// let j = 2;
 
-// Write code here. 
+function matrix(n){
+    let result = new Array(n).fill().map(() => new Array(n).fill("*"))
+    let counter = 1
+    let startCol = 0
+    let endCol = n - 1
+    let startRow = 0
+    let endRow = n - 1
+
+    while(startCol <= endCol && startRow <= endRow){
+
+        for(let i = startCol;i<=endCol;i++){
+            result[startRow][i] = counter
+            counter ++
+        }
+
+        startRow++
+
+        for(let i = startRow;i<=endRow;i++){
+            result[i][endCol] = counter
+            counter++
+        }
+
+        endCol--
+
+        for(let i = endCol; i>=startCol;i--){
+            result[endRow][i] = counter
+            counter++
+        }
+
+        endRow--
+
+        for(i = endRow; i>=startRow;i--){
+            result[i][startCol]= counter
+            counter++
+        }
+
+        startCol ++
+    }
+
+
+    return result
+}
+matrix(n);
+
+let a = 4;
+let b = 2;
+
+console.log(matrix(n)[a-1][b-1]);
+
+
