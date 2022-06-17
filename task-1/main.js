274e460fe6c9627c4b1dc5bab74aaafb2f7c041f
Aslan Mammadov <mammadovaslan21@gmail.com>
2022-06-17 10:01:40 +0400

    task

--- task-1/main.js
+++ task-1/main.js
@@ -5,4 +5,17 @@ let matrix = [
     [0, 1, -3]
 ]
 
-// Write code here. 
+let count = 0
+let result = 0
+
+for(let i = 0;i<matrix.length;i++){
+    for(let j = 0;j<matrix.length;j++){
+        if(matrix[i][j] <0 && matrix[i][j]%2==0){
+            count++
+            result +=matrix[i][j]
+        }
+    }
+}
+
+console.log(count);
+console.log(result);
