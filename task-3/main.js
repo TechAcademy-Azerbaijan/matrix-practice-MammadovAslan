453a3c31e24f7ab02a8dcfba24829b1f921025ec
Aslan Mammadov <mammadovaslan21@gmail.com>
2022-06-17 10:24:37 +0400

    task

--- task-3/main.js
+++ task-3/main.js
@@ -6,5 +6,19 @@ let matrix = [
     [4, 6, 1, 2, 3],
     [4, 5, 6, 7, 7]
 ]
+let min = matrix[0][0]
 
-// Write code here. 
+for(let i = 0;i<matrix.length;i++){
+    for(let j = 0;j<matrix.length;j++){
+        if(matrix[i][j] < min){
+            min = matrix[i][j]
+        }
+    }
+
+    for(let j = 0;j<matrix.length;j++){
+        if(matrix[i][j] == min){
+            console.log(i+1);
+        }
+    }
+}
+
