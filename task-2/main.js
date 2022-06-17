24d5d7ddaa12d220767cfeb5ef7c4a5d035c901b
Aslan Mammadov <mammadovaslan21@gmail.com>
2022-06-17 10:16:15 +0400

    task

--- task-2/main.js
+++ task-2/main.js
@@ -7,7 +7,24 @@ let matrix = [
     [4, 5, 6, 7]
 ];
 
-let r = 3;
+let r = 3;          
 let c = 2;
+let first = []
+let second = []
+let third = []
 
-// Write code here. 
+for(let i = 0;i<r;i++){
+    for(let j = 0;j<c;j++){
+        if(first.length !== 2){
+            first.push(matrix[i][j])
+        }else if(second.length !==2){
+            second.push(matrix[i][j])
+        }else if(third.length !== 2){
+            third.push(matrix[i][j])
+        }
+    }
+}
+
+console.log(first.join(' '));
+console.log(second.join(' '));
+console.log(third.join(' '));
