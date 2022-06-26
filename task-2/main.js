16e9876348145d1de60c83923310da45f871488c
Aslan Mammadov <mammadovaslan21@gmail.com>
2022-06-26 21:43:14 +0400

    task

--- task-2/main.js
+++ task-2/main.js
@@ -9,25 +9,16 @@ let matrix = [
 
 let r = 3;          
 let c = 2;
-let first = []
-let second = []
-let third = []
+let str = ''
+
 
 for(let i = 0;i<r;i++){
     for(let j = 0;j<c;j++){
-        if(first.length !== 2){
-            first.push(matrix[i][j])
-        }else if(second.length !==2){
-            second.push(matrix[i][j])
-        }else if(third.length !== 2){
-            third.push(matrix[i][j])
-        }
+        str += matrix[i][j] + ' ';
     }
+    str = str.trim()
+    str += '\n';
 }
-
-console.log(first.join(' '));
-console.log(second.join(' '));
-console.log(third.join(' ')); 
-
+console.log(str);
 
 
